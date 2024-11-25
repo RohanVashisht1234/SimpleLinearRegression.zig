bd183db16bd2d5aeb9e208178b0e35800b26759d
Rohan Vashisht <rohanprogrammer1@gmail.com>
2024-11-26 00:25:15 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# SimpleLinearRegression Library for Zig programming language
+
+## How to use?
+
+- Run the fetch command:
+
+```zig
+zig fetch --save "https://github.com/RohanVashisht1234/"
+```
+
+- Add the following to your build.zig file just above `b.installArtifact(exe);`:
+
+```zig
+const SimpleLinearRegression = b.dependency("SimpleLinearRegression", .{
+    .target = target,
+    .optimize = optimize,
+});
+
+exe.root_module.addImport("SimpleLinearRegression", SimpleLinearRegression.module("SimpleLinearRegression"));
+```
+
+- Now, you can add the following code to `src/main.zig`:
+```zig
+const std = @import("std");
+const SimpleLinearRegression = @import("SimpleLinearRegression.zig");
+
+pub fn main() void {
+    const data = &.{
+        // x , y
+        .{ 1.2, 39344.0 },
+        .{ 1.4, 46206.0 },
+        // ...
+    };
+    var SLR = SimpleLinearRegression.init(data);
+    const y = SLR.predict(11.0);
+    std.debug.print("Y: {d}\n", .{y});
+    std.debug.print("M: {d}\n", .{SLR.m});
+    std.debug.print("C: {d}\n", .{SLR.c});
+}
+```
+- You can check the examples folder for better examples.
---
+++ src/SimpleLinearRegression.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+// private
+const SLRData = struct {
+    data: []const [2]f32,
+    m: f32,
+    c: f32,
+    const Self = @This();
+    pub fn predict(self: *Self, x: f32) f32 {
+        const y = self.m * x + self.c;
+        return y;
+    }
+};
+
+pub fn init(data: []const [2]f32) SLRData {
+    const m_and_c = get_m_and_c(data);
+    const m = m_and_c[0];
+    const c = m_and_c[1];
+    return SLRData{ .data = data, .m = m, .c = c };
+}
+
+fn get_m_and_c(data: []const [2]f32) [2]f32 {
+    var sum_x: f32 = 0;
+    var sum_y: f32 = 0;
+    for (data) |set| {
+        const X = set[0];
+        sum_x += X;
+        const Y = set[1];
+        sum_y += Y;
+    }
+    const data_len = @as(f32, @floatFromInt(data.len));
+    const avg_x = sum_x / data_len;
+    const avg_y = sum_y / data_len;
+
+    var numerator_sum: f32 = 0;
+    for (data) |set| {
+        const X_i = set[0];
+        const Y_i = set[1];
+        numerator_sum += (X_i - avg_x) * (Y_i - avg_y);
+    }
+    var denominator_sum: f32 = 0;
+    for (data) |set| {
+        const X_i = set[0];
+        denominator_sum += std.math.pow(f32, (X_i - avg_x), 2.0);
+    }
+    const m = numerator_sum / denominator_sum;
+    const c = avg_y - (m * avg_x);
+
+    const result = [2]f32{ m, c };
+    return result;
+}
